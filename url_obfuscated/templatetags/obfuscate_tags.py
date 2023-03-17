0b5f09f5f8f2c05088714722b6d3f36143363d7a
Dhyanikumar <dhyanipalan@gmail.com>
2023-03-17 10:58:28 +0530

    removed debug comments

--- url_obfuscated/templatetags/obfuscate_tags.py
+++ url_obfuscated/templatetags/obfuscate_tags.py
@@ -5,12 +5,7 @@ register = template.Library()
 
 @register.filter(name='obfuscate')
 def obfuscate_url_parameter(value):
-    print('in obfuscate_url_parameter tags')
     if isinstance(value, int):
-        print('Integer-----', value, type(value))
         return obfuscate(str(value))
     else:
-        print('String-----', value, type(value))
-        temp = value.encode('utf8')
-        print(temp, type(temp))
         return obfuscate(str(value))
